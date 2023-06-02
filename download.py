a2092ebba077824b54fd4308e76922f61bdd55c7
FlyingPTT <43295479+FlyingPTT@users.noreply.github.com>
2023-06-03 05:31:34 +0800

    1

--- download.py
+++ download.py
@@ -25,8 +25,8 @@ def download_pdf(link, location, name):
     try:
         response = requests.get(link)
         with open(os.path.join(location, name), 'wb') as f:
-        	f.write(response.content)
-        	f.close()
+            f.write(response.content)
+            f.close()
     except HTTPError:
         print('>>> Error 404: cannot be downloaded!\n') 
         raise   
